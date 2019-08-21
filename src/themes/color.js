b5c32a94c640ca4bdb659d3eeed0438f90c12031
Rati Kirvalidze <rati.kirvalidze10@gmail.com>
2019-08-21 11:35:03 +0400

    updated themes

--- src/themes/color.js
+++ src/themes/color.js
@@ -2,5 +2,7 @@ export default {
   primary: '#6C63FF',
   dark: '#1A1A1A',
   gray: `rgba(26, 26, 26, 0.3)`,
-  white: '#FFFFFF'
+  white: '#FFFFFF',
+  purple: '#8000E8',
+  lightGrey: '#F1F1F1'
 }
